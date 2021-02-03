b45eb870d2f4194ae5554d7c17eebc7d8c311cb4
nozomail <nozomi.mail@icloud.com>
2021-02-03 17:15:59 +1100

    Update app page styling

--- pages/about.tsx
+++ pages/about.tsx
@@ -12,21 +12,31 @@ type AboutProps = {
 export default function About({ img, text, tools }: AboutProps) {
   return (
     <Layout pageTitle="About">
-      <h1 className="w-32 rounded-full mx-auto shadow-sw">
-        <img src={img} alt="Nozomi Mail" className="w-32 ring-4 rounded-full ring-white" />
-      </h1>
-      <p className="mt-8">{text}</p>
-      <h2 className="font-display text-2xl text-shadow-sw text-center mt-20">Skills and Tools</h2>
-      {tools.map((tool, i) => (
-        <div key={i} className="border border-white rounded-md mt-8">
-          <h3 className="bg-white bg-opacity-30 py-1 px-4 text-center">{tool.category}</h3>
-          <ul className="list list-disc list-inside flex flex-wrap py-2">
-            {tool.names.map((name, j) => (
-              <li key={j} className="py-1 px-4">{name}</li>
-            ))}
-          </ul>
+      <section className="max-w-screen-sm mx-auto">
+        <h1 className="w-32 rounded-full mx-auto shadow-sw">
+          <img src={img} alt="Nozomi Mail" className="w-32 ring-4 rounded-full ring-white" />
+        </h1>
+        <p className="mt-8" dangerouslySetInnerHTML={{ __html: text }}></p>
+      </section>
+      <section>
+        <h2 className="font-display text-2xl text-shadow-sw text-center mt-20">Skills and Tools</h2>
+        <div className="sm:flex flex-wrap -mx-2">
+          {tools.map((tool, i) => (
+            <div key={i} className="sm:w-1/2 md:w-1/3 lg:w-1/4 mt-8 px-2">
+              <div className="sm:h-full border border-white rounded-md">
+                <h3 className="bg-white bg-opacity-30 py-1 px-4 text-center">{tool.category}</h3>
+                <ul className="list list-disc list-inside flex flex-wrap py-2">
+                  {tool.names.map((name, j) => (
+                    <li key={j} className="py-1 px-4">
+                      {name}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
+      </section>
     </Layout>
   );
 }
@@ -34,7 +44,7 @@ export default function About({ img, text, tools }: AboutProps) {
 export async function getStaticProps() {
   const res = await fetch(`${process.env.API_URL}/about/`);
   const data = await res.json();
-  const {img, text, tools} = data[0]
+  const { img, text, tools } = data[0];
 
   return {
     props: {
